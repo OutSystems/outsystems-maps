ecd8ab2a9caa9bb4c25dc0648c23fb3383f574c5
tmbp95 <correio.tiago@gmail.com>
2021-05-04 16:59:33 +0100

    Conversion for Address to Coordinates changed to Location to Coordinates

--- code/src/GoogleProvider/Helper/Conversions.ts
+++ code/src/GoogleProvider/Helper/Conversions.ts
@@ -1,31 +1,37 @@
 namespace GoogleProvider.Helper.Conversions {
 
     /**
-     * Promise that will retrive the coordinates of a specific address via Google Maps API
+     * Promise that will retrive the coordinates for a specific location via Google Maps API
      * 
-     * @param address Address of a location to convert to coordinates
+     * @param location Location to convert to coordinates
      * @param apiKey Google API Key 
      * @returns Promise that will retrieve the coordinates
      */
-    export function ConvertAddressToCoordinates(
-        address: string,
+    export function ConvertLocationToCoordinates(
+        location: string,
         apiKey: string
     ): any {
-        if (address.search(/[a-zA-Z]/g) === -1) {
+        if(location.length === 0) {
+            throw new Error("The address can't be empty.");
+        }
+        // If the location is a set of coordinates
+        if (location.search(/[a-zA-Z]/g) === -1) {
             let latitude: number;
             let longitude: number;
-            if (address.indexOf(',') > -1) {
-                latitude = parseFloat(address.split(',')[0].replace(' ', ''));
-                longitude = parseFloat(address.split(',')[1].replace(' ', ''));
+            if (location.indexOf(',') > -1) {
+                latitude = parseFloat(location.split(',')[0].replace(' ', ''));
+                longitude = parseFloat(location.split(',')[1].replace(' ', ''));
 
                 return new Promise((resolve, reject) => {
                     resolve({lat: latitude, lng: longitude});
                 });
                 
             }
-        } else {
-            address = address.replace(/[^a-zA-Z0-9 ]/g, "");
-            return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`)
+        } 
+        // If the location is an address
+        else {
+            location = location.replace(/[^a-zA-Z0-9 ]/g, "");
+            return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${apiKey}`)
             .then(response => {
                 if(response.ok) {
                     return response.json();
@@ -34,8 +40,11 @@ namespace GoogleProvider.Helper.Conversions {
                 }
             })
             .then(json => {
-                const location = json.results[0].geometry.location;
-                return {lat: location.lat, lng: location.lng};
+                if(json.results.length === 0){
+                    throw new Error('Server response wasn\'t OK');
+                }
+                const loc = json.results[0].geometry.location;
+                return {lat: loc.lat, lng: loc.lng};
             });
         }    
     }
