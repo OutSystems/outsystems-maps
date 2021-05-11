69403c1e4af553bcb80f3aa33f32aee235e4a911
tmbp95 <correio.tiago@gmail.com>
2021-05-11 16:52:57 +0100

    Lint Fixes

--- code/src/GoogleProvider/Helper/Conversions.ts
+++ code/src/GoogleProvider/Helper/Conversions.ts
@@ -1,18 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace GoogleProvider.Helper.Conversions {
-
     /**
      * Promise that will retrive the coordinates for a specific location via Google Maps API
-     * 
+     *
      * @param location Location to convert to coordinates
-     * @param apiKey Google API Key 
+     * @param apiKey Google API Key
      * @returns Promise that will retrieve the coordinates
      */
     export function ConvertToCoordinates(
         location: string,
         apiKey: string
     ): Promise<OSFramework.OSStructures.OSMap.Coordinates> {
-        if(location.length === 0) {
-            throw new Error("Invalid location.");
+        if (location === undefined || location.trim().length === 0) {
+            console.error(
+                'Invalid location. Using the default location -> 55 Thomson Pl 2nd floor, Boston, MA 02210, United States'
+            );
+            return new Promise((resolve) => {
+                resolve(OSFramework.Helper.Constants.defaultMapCenter);
+            });
         }
         // If the location is a set of coordinates
         if (location.search(/[a-zA-Z]/g) === -1) {
@@ -22,30 +27,31 @@ namespace GoogleProvider.Helper.Conversions {
                 latitude = parseFloat(location.split(',')[0].replace(' ', ''));
                 longitude = parseFloat(location.split(',')[1].replace(' ', ''));
 
-                return new Promise((resolve, reject) => {
-                    resolve({lat: latitude, lng: longitude});
+                return new Promise((resolve) => {
+                    resolve({ lat: latitude, lng: longitude });
                 });
-                
             }
-        } 
+        }
         // If the location is an address
         else {
-            location = location.replace(/[^a-zA-Z0-9 ]/g, "");
-            return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${apiKey}`)
-            .then(response => {
-                if(response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Server response wasn\'t OK');
-                }
-            })
-            .then(json => {
-                if(json.results.length === 0){
-                    throw new Error('Server response wasn\'t OK');
-                }
-                const loc = json.results[0].geometry.location;
-                return {lat: loc.lat, lng: loc.lng};
-            });
-        }    
+            location = location.replace(/[^a-zA-Z0-9 ]/g, '');
+            return fetch(
+                `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${apiKey}`
+            )
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error("Server response wasn't OK");
+                    }
+                })
+                .then((json) => {
+                    if (json.results.length === 0) {
+                        throw new Error("Server response wasn't OK");
+                    }
+                    const loc = json.results[0].geometry.location;
+                    return { lat: loc.lat, lng: loc.lng };
+                });
+        }
     }
 }
