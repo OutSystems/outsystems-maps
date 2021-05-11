ff1be9981120464ac0950a353f471fbeb9414734
tmbp95 <correio.tiago@gmail.com>
2021-05-11 16:51:46 +0100

    Fixed the Center, Offset and Zoom features in order to change with AutoZoom

--- code/src/GoogleProvider/Features/Offset.ts
+++ code/src/GoogleProvider/Features/Offset.ts
@@ -1,75 +1,39 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace GoogleProvider.Feature {
     export class Offset
-        implements
-            OSFramework.Feature.IOffset,
-            OSFramework.Interface.IBuilder {
-        private _map: GoogleProvider.Map.IMapGoogle;
+        implements OSFramework.Feature.IOffset, OSFramework.Interface.IBuilder {
+        private _map: Map.IMapGoogle;
+        /** Current offset of the Map */
         private _offset: OSFramework.OSStructures.OSMap.Offset;
-        private _autoFit: boolean;
 
-        constructor(map: GoogleProvider.Map.IMapGoogle, offset: OSFramework.OSStructures.OSMap.Offset, autofit: boolean) {
+        constructor(
+            map: Map.IMapGoogle,
+            offset: OSFramework.OSStructures.OSMap.Offset
+        ) {
             this._map = map;
-            this._offset = offset || {offsetX: 0, offsetY: 0};
-            this._autoFit = autofit || false;
+            this._offset = offset || { offsetX: 0, offsetY: 0 };
         }
 
         public get getOffset(): OSFramework.OSStructures.OSMap.Offset {
             return this._offset;
         }
 
-        public get isAutoFit(): boolean {
-            return this._autoFit;
-        }
-
-        public build(): void {
-            this.setOffset(this._offset);
-        }
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        public build(): void {}
 
         public setOffset(value: OSFramework.OSStructures.OSMap.Offset): void {
+            console.log('offset');
             this._offset = {
-                offsetX: value.offsetX || 0, 
+                offsetX: value.offsetX || 0,
                 offsetY: value.offsetY || 0
-            }
-            this._map.provider.panTo(this._map.features.center.getCenter());
-            this._map.provider.panBy(this._offset.offsetX, this._offset.offsetY);
-        }
-
-        public setAutoFit(state: boolean): void {
-            this._autoFit = state;
-            if(state === true) {
-                // Set behavior for autofit mode
-                const bounds = new google.maps.LatLngBounds();
-                const map = this._map;
-                const markers = map.getMarkers();
-                
-                let loc: google.maps.LatLng;
-                if (markers.length === 1) {
-                    loc = new google.maps.LatLng(
-                        markers[0].provider.position.lat(),
-                        markers[0].provider.position.lng()
-                    );
-                    bounds.extend(loc);
-                    map.provider.fitBounds(bounds);
-                    map.changeProperty('center', loc);
-                    map.changeProperty('zoom', 8);
-                } else if (markers.length >= 2) {
-                    markers.forEach(function (item) {
-                        loc = new google.maps.LatLng(
-                            item.provider.position.lat(),
-                            item.provider.position.lng()
-                        );
-                        bounds.extend(loc);
-                    });
-                    map.provider.fitBounds(bounds);
-                    map.provider.panToBounds(bounds);
-                } else {
-                    return;
-                }
-            } else {
-                // Set behavior for disabled autofit mode
-                return;
-            }
+            };
+            this._map.provider.panTo(
+                this._map.features.center.getCurrentCenter()
+            );
+            this._map.provider.panBy(
+                this._offset.offsetX,
+                this._offset.offsetY
+            );
         }
     }
 }
