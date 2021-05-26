b006dcf1896ad9528d9aba09dcd8f2587e5cfb2e
tmbp95 <correio.tiago@gmail.com>
2021-05-26 12:49:07 +0100

    remove File devOverride

--- code/devOverride.bat
+++
@@ -1,7 +0,0 @@
-:again 
-    set /p var=Please enter path to the file to continue: 
-    IF EXIST %var%. goto teste2
-    
-:teste2
-    for /f %%i in ('dir %var%\Map_Regressions_MapsV2\scripts\OutSystemsMaps_MapsV2.OutSystemsMaps.js%%3f* /b/a-d/od/t:c') do set LAST=%%i
-    tsc -w --outFile %var%\Map_Regressions_MapsV2\scripts\%LAST%
