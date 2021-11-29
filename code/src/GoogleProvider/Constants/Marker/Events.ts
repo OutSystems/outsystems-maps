9454d0fd4abce4ec8379580e8e846cb5d9b5248f
Tiago Pereira <tiago.miguel.pereira@outsystems.com>
2021-11-29 15:06:41 +0000

    Marker Leaflet and GoogleMaps events update

--- code/src/GoogleProvider/Constants/Marker/Events.ts
+++ code/src/GoogleProvider/Constants/Marker/Events.ts
@@ -1,30 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace GoogleProvider.Constants.Marker {
     /**
-     * Array of strings that define the available Provider Events
+     * Enum that defines the available Provider Events
      */
-    export const Events = [
-        'animation_changed',
-        'click',
-        'clickable_changed',
-        'contextmenu',
-        'cursor_changed',
-        'dblclick',
-        'drag',
-        'dragend',
-        'draggable_changed',
-        'dragstart',
-        'flat_changed',
-        'icon_changed',
-        'mousedown',
-        'mouseout',
-        'mouseover',
-        'mouseup',
-        'position_changed',
-        'rightclick',
-        'shape_changed',
-        'title_changed',
-        'visible_changed',
-        'zindex_changed'
-    ];
+    export enum ProviderEventNames {
+        animation_changed = 'animation_changed',
+        click = 'click',
+        clickable_changed = 'clickable_changed',
+        contextmenu = 'contextmenu',
+        cursor_changed = 'cursor_changed',
+        dblclick = 'dblclick',
+        drag = 'drag',
+        dragend = 'dragend',
+        draggable_changed = 'draggable_changed',
+        dragstart = 'dragstart',
+        flat_changed = 'flat_changed',
+        icon_changed = 'icon_changed',
+        mousedown = 'mousedown',
+        mouseout = 'mouseout',
+        mouseover = 'mouseover',
+        mouseup = 'mouseup',
+        position_changed = 'position_changed',
+        rightclick = 'rightclick',
+        shape_changed = 'shape_changed',
+        title_changed = 'title_changed',
+        visible_changed = 'visible_changed',
+        zindex_changed = 'zindex_changed'
+    }
 }
---
+++ code/src/LeafletProvider/Constants/Marker/Events.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace LeafletProvider.Constants.Marker {
+    /**
+     * Enum that defines the available Provider Events
+     */
+    export enum ProviderEventNames {
+        click = 'click',
+        contextmenu = 'contextmenu',
+        rightclick = 'contextmenu',
+        dblclick = 'dblclick',
+        drag = 'drag',
+        dragend = 'dragend',
+        dragstart = 'dragstart',
+        mousedown = 'mousedown',
+        mouseout = 'mouseout',
+        mouseover = 'mouseover',
+        mouseup = 'mouseup'
+    }
+}
