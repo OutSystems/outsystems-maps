f9af4b817964607305356b3c27b3d0b995a0d687
Gabriel Lundgren <gabriel.lundgren@outsystems.com>
2022-08-19 11:10:42 +0200

    changing provider namespace

--- src/Providers/Maps/Leaflet/Configuration/DrawingTools/DrawConfig.ts
+++ src/Providers/Maps/Leaflet/Configuration/DrawingTools/DrawConfig.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../../../OSFramework/Configuration/AbstractConfiguration.ts" />
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-namespace Provider.Leaflet.Configuration.DrawingTools {
+namespace Provider.Maps.Leaflet.Configuration.DrawingTools {
     export class DrawConfig
         extends OSFramework.Maps.Configuration.AbstractConfiguration
         implements OSFramework.Maps.Configuration.IConfigurationTool
