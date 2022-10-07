36fa05a1b37aed8ae5f662220cfdbf32f58e4927
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-10-07 10:09:50 +0100

    Update IShape.ts

--- src/OSFramework/Maps/Feature/IShape.ts
+++ src/OSFramework/Maps/Feature/IShape.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.Maps.Feature {
     export interface IShape {
+        // Check if the marker is inside shape and expose the result
         containsLocation(
             mapId: string,
             shapeId: string,
