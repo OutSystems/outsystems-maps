30aa5b0b170c240b9a1c9d2a2eed78bacc4c776d
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-10-14 14:38:57 +0100

    Fixing code smelss #3

--- src/Providers/Maps/Leaflet/Features/Shape.ts
+++ src/Providers/Maps/Leaflet/Features/Shape.ts
@@ -37,78 +37,82 @@ namespace Provider.Maps.Leaflet.Feature {
                         OutSystems.Maps.MapAPI.ShapeManager.GetShapeById(
                             shapeId
                         );
-                    if (
-                        shape.type === OSFramework.Maps.Enum.ShapeType.Polyline
-                    ) {
-                        // The Polyline is an unsupported shape to use the ContainsLocation API
-                        returnMessage = {
-                            isSuccess: renderedSuccessfully,
-                            code: OSFramework.Maps.Enum.Unsupported.code,
-                            message: OSFramework.Maps.Enum.Unsupported.message
-                        };
-                        return returnMessage;
-                    }
-                    // Exception added for circle
-                    if (shape.type === OSFramework.Maps.Enum.ShapeType.Circle) {
-                        const circleCenter = shape.provider.getLatLng();
-                        const circleRadius = shape.provider.getRadius();
-                        marker = L.latLng(marker.Lat, marker.Lng);
-                        const distanceBetweenPoints = map.provider.distance(
-                            marker,
-                            circleCenter
-                        );
 
-                        // Check if circle contains the marker coordinates
-                        isInsideShape = distanceBetweenPoints < circleRadius;
-                        renderedSuccessfully = true;
-                    } else {
-                        // Check if shape contains marker based on shape type
-                        polyPoints = shape.provider.getLatLngs()[0];
+                    switch (shape.type) {
+                        case OSFramework.Maps.Enum.ShapeType.Polyline:
+                            // The Polyline is an unsupported shape to use the ContainsLocation API
 
-                        // axis points to compare the shape coordinates
-                        let xi;
-                        let yi;
-                        let xj;
-                        let yj;
-                        let intersect;
-                        let previousPolyPoint = polyPoints.length - 1;
+                            returnMessage = {
+                                isSuccess: renderedSuccessfully,
+                                code: OSFramework.Maps.Enum.Unsupported.code,
+                                message:
+                                    OSFramework.Maps.Enum.Unsupported.message
+                            };
+                            break;
+                        case OSFramework.Maps.Enum.ShapeType.Circle: {
+                            const circleCenter = shape.provider.getLatLng();
+                            const circleRadius = shape.provider.getRadius();
+                            marker = L.latLng(marker.Lat, marker.Lng);
+                            const distanceBetweenPoints = map.provider.distance(
+                                marker,
+                                circleCenter
+                            );
 
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        for (const { index, value } of polyPoints.map(
-                            (value, index) => ({
-                                index,
-                                value
-                            })
-                        )) {
-                            // Check if the index of polyPoints is higher then the length
-                            // If true, set it to the first index element
-                            if (previousPolyPoint > polyPoints.length - 1) {
-                                previousPolyPoint = index - 1;
-                            }
-
-                            // Set the axis coordinates to compare
-                            xi = polyPoints[index].lat;
-                            yi = polyPoints[index].lng;
-                            xj = polyPoints[previousPolyPoint].lat;
-                            yj = polyPoints[previousPolyPoint].lng;
-
-                            // Check the intersection of the points based on axis coordinates
-                            intersect =
-                                yi > marker.Lng !== yj > marker.Lng &&
-                                marker.Lat <
-                                    ((xj - xi) * (marker.Lng - yi)) /
-                                        (yj - yi) +
-                                        xi;
-
-                            // Check if has intersection with each point on shape
-                            if (intersect) {
-                                isInsideShape = !isInsideShape;
-                            }
-
-                            // Increment the previousPolyPoint to compare with the next coordinate
-                            previousPolyPoint = ++previousPolyPoint;
+                            // Check if circle contains the marker coordinates
+                            isInsideShape =
+                                distanceBetweenPoints < circleRadius;
+                            renderedSuccessfully = true;
+                            break;
                         }
-                        renderedSuccessfully = true;
+                        default: {
+                            // Check if shape contains marker based on shape type
+                            polyPoints = shape.provider.getLatLngs()[0];
+
+                            // axis points to compare the shape coordinates
+                            let xi;
+                            let yi;
+                            let xj;
+                            let yj;
+                            let intersect;
+                            let previousPolyPoint = polyPoints.length - 1;
+
+                            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                            for (const { index } of polyPoints.map(
+                                (value, index) => ({
+                                    value,
+                                    index
+                                })
+                            )) {
+                                // Check if the index of polyPoints is higher then the length
+                                // If true, set it to the first index element
+                                if (previousPolyPoint > polyPoints.length - 1) {
+                                    previousPolyPoint = index - 1;
+                                }
+
+                                // Set the axis coordinates to compare
+                                xi = polyPoints[index].lat;
+                                yi = polyPoints[index].lng;
+                                xj = polyPoints[previousPolyPoint].lat;
+                                yj = polyPoints[previousPolyPoint].lng;
+
+                                // Check the intersection of the points based on axis coordinates
+                                intersect =
+                                    yi > marker.Lng !== yj > marker.Lng &&
+                                    marker.Lat <
+                                        ((xj - xi) * (marker.Lng - yi)) /
+                                            (yj - yi) +
+                                            xi;
+
+                                // Check if has intersection with each point on shape
+                                if (intersect) {
+                                    isInsideShape = !isInsideShape;
+                                }
+
+                                // Increment the previousPolyPoint to compare with the next coordinate
+                                previousPolyPoint = ++previousPolyPoint;
+                            }
+                            renderedSuccessfully = true;
+                        }
                     }
                 } else {
                     const shapeCoordinatesList = JSON.parse(coordinatesList);
