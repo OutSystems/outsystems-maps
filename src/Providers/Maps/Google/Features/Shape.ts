a90828a6a01d0d6eb94adadfef47f42f6eee8a25
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-10-14 17:19:47 +0100

    Update Shape.ts

--- src/Providers/Maps/Google/Features/Shape.ts
+++ src/Providers/Maps/Google/Features/Shape.ts
@@ -6,6 +6,53 @@ namespace Provider.Maps.Google.Feature {
             OSFramework.Maps.Interface.IBuilder,
             OSFramework.Maps.Interface.IDisposable
     {
+        private _isInsideShape: boolean;
+        private _map: OSFramework.Maps.OSMap.IMap;
+        private _markerCoordinates;
+        private _polyPoints = [];
+        private _returnObjCode: string;
+        private _returnObjMessage: string;
+        private _returnObjSuccess: boolean;
+        private _shape: OSFramework.Maps.Shape.IShape;
+
+        // Method for shape circle exception
+        private _shapeCircle(): void {
+            const circleRadius = this._shape.provider.getRadius();
+            const circleCenter = this._shape.provider.getCenter();
+            const distanceBetweenPoints =
+                google.maps.geometry.spherical.computeDistanceBetween(
+                    this._markerCoordinates,
+                    circleCenter
+                );
+
+            this._isInsideShape = distanceBetweenPoints <= circleRadius;
+            this._returnObjSuccess = true;
+            this._returnObjCode = OSFramework.Maps.Enum.Success.code;
+            this._returnObjMessage = this._isInsideShape.toString();
+        }
+
+        // Method to apply the default calculations for shapes
+        private _shapeDefault(): void {
+            this._isInsideShape = google.maps.geometry.poly.containsLocation(
+                this._markerCoordinates,
+                this._map.getShape(this._shape.widgetId).provider
+            );
+            this._returnObjSuccess = true;
+            this._returnObjCode = OSFramework.Maps.Enum.Success.code;
+            this._returnObjMessage = this._isInsideShape.toString();
+        }
+
+        // Method for shape rectangle exception
+        private _shapeRectangle(): void {
+            this._isInsideShape = this._map
+                .getShape(this._shape.widgetId)
+                .provider.getBounds()
+                .contains(this._markerCoordinates);
+            this._returnObjSuccess = true;
+            this._returnObjCode = OSFramework.Maps.Enum.Success.code;
+            this._returnObjMessage = this._isInsideShape.toString();
+        }
+
         public build(): void {
             //
         }
@@ -16,69 +63,50 @@ namespace Provider.Maps.Google.Feature {
             pointCoordinates: string,
             coordinatesList: string
         ): OSFramework.Maps.OSStructures.ReturnMessage {
-            const map = OutSystems.Maps.MapAPI.MapManager.GetMapById(mapId);
+            this._map = OutSystems.Maps.MapAPI.MapManager.GetMapById(mapId);
             // Set the default return message to prevent different else if's
-            let returnMessage: OSFramework.Maps.OSStructures.ReturnMessage = {
-                isSuccess: false,
-                code: OSFramework.Maps.Enum.ErrorCodes.CFG_InvalidMapId
-            };
+            this._returnObjSuccess = false;
+            this._returnObjCode =
+                OSFramework.Maps.Enum.ErrorCodes.CFG_InvalidMapId;
 
             // Check if map exists
-            if (map) {
-                let isInsideShape = false;
-                let renderedSuccessfully = false;
+            if (this._map) {
+                this._isInsideShape = false;
 
-                const markerCoordinates = JSON.parse(pointCoordinates);
-                const markerLocation = new google.maps.LatLng(
-                    markerCoordinates.Lat,
-                    markerCoordinates.Lng
+                // Convert to JSON the marker location before to convert to google format
+                this._markerCoordinates = JSON.parse(pointCoordinates);
+                this._markerCoordinates = new google.maps.LatLng(
+                    this._markerCoordinates.Lat,
+                    this._markerCoordinates.Lng
                 );
 
                 // Check if marker is inside shape based on shape element
                 if (shapeId) {
-                    const shape =
+                    this._shape =
                         OutSystems.Maps.MapAPI.ShapeManager.GetShapeById(
                             shapeId
                         );
 
                     // Check if shape contains marker based on shape type
-                    if (
-                        shape.type === OSFramework.Maps.Enum.ShapeType.Polyline
-                    ) {
-                        // The Polyline is an unsupported shape to use the ContainsLocation API
-                        returnMessage = {
-                            isSuccess: renderedSuccessfully,
-                            code: OSFramework.Maps.Enum.Unsupported.code,
-                            message: OSFramework.Maps.Enum.Unsupported.message
-                        };
-                        return returnMessage;
-                    } else if (
-                        shape.type === OSFramework.Maps.Enum.ShapeType.Rectangle
-                    ) {
-                        isInsideShape = map
-                            .getShape(shapeId)
-                            .provider.getBounds()
-                            .contains(markerLocation);
-                    } else if (
-                        shape.type === OSFramework.Maps.Enum.ShapeType.Circle
-                    ) {
-                        const circleRadius = shape.provider.getRadius();
-                        const circleCenter = shape.provider.getCenter();
-                        const distanceBetweenPoints =
-                            google.maps.geometry.spherical.computeDistanceBetween(
-                                markerLocation,
-                                circleCenter
-                            );
+                    switch (this._shape.type) {
+                        case OSFramework.Maps.Enum.ShapeType.Polyline:
+                            // The Polyline is an unsupported shape to use the ContainsLocation API
+                            this._returnObjCode =
+                                OSFramework.Maps.Enum.Unsupported.code;
+                            this._returnObjMessage =
+                                OSFramework.Maps.Enum.Unsupported.message;
+                            break;
+                        case OSFramework.Maps.Enum.ShapeType.Rectangle:
+                            this._shapeRectangle();
+                            break;
+                        case OSFramework.Maps.Enum.ShapeType.Circle:
+                            this._shapeCircle();
+                            break;
 
-                        isInsideShape = distanceBetweenPoints <= circleRadius;
-                    } else {
-                        isInsideShape =
-                            google.maps.geometry.poly.containsLocation(
-                                markerLocation,
-                                map.getShape(shapeId).provider
-                            );
+                        default: {
+                            this._shapeDefault();
+                        }
                     }
-                    renderedSuccessfully = true;
                 } else {
                     const shapeCoordinatesList = JSON.parse(
                         coordinatesList.toLowerCase()
@@ -90,24 +118,21 @@ namespace Provider.Maps.Google.Feature {
                             paths: shapeCoordinatesList
                         });
 
-                        isInsideShape =
+                        this._isInsideShape =
                             google.maps.geometry.poly.containsLocation(
-                                markerLocation,
+                                this._markerCoordinates,
                                 newShape
                             );
                     }
                 }
-                returnMessage = {
-                    isSuccess: renderedSuccessfully,
-                    code: renderedSuccessfully
-                        ? OSFramework.Maps.Enum.Success.code
-                        : OSFramework.Maps.Enum.ErrorCodes
-                              .CFG_InvalidPolygonShapeLocations,
-                    message: renderedSuccessfully
-                        ? isInsideShape.toString()
-                        : OSFramework.Maps.Enum.Success.message
-                };
             }
+
+            // Set the return message to expose values
+            const returnMessage = {
+                isSuccess: this._returnObjSuccess,
+                code: this._returnObjCode,
+                message: this._returnObjMessage
+            };
 
             return returnMessage;
         }
