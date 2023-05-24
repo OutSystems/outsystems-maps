1e36a2da3336b686c393c9e8d1137d05f8c4a547
rug <rmfgoncalves@gmail.com>
2023-05-24 13:14:44 +0200

    Fixing messages left from merge (#135)

--- src/Providers/Maps/Leaflet/Configuration/Shape/RectangleShapeConfig.ts
+++ src/Providers/Maps/Leaflet/Configuration/Shape/RectangleShapeConfig.ts
@@ -1,11 +1,7 @@
 /// <reference path="./FilledShapeConfig.ts" />
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-<<<<<<<< HEAD:src/Providers/Maps/Leaflet/Configuration/Shape/RectangleShapeConfig.ts
 namespace Provider.Maps.Leaflet.Configuration.Shape {
-========
-namespace Provider.Maps.Google.Configuration.Shape {
->>>>>>>> rc1.6.3:src/Providers/Maps/Google/Configuration/Shape/RectangleShapeConfig.ts
     export class RectangleShapeConfig extends FilledShapeConfig {
         public bounds: string;
 
