5e1267977b2e8e81a78b115719dcbe9599d94eb0
Marcelino <brunoapmarcelino@gmail.com>
2023-12-11 14:25:24 +0000

    Fix duplicated methods on Circle

--- src/Providers/Maps/Leaflet/Shape/Circle.ts
+++ src/Providers/Maps/Leaflet/Shape/Circle.ts
@@ -113,19 +113,6 @@ namespace Provider.Maps.Leaflet.Shape {
             };
         }
 
-        protected getShapeCoordinates(): OSFramework.Maps.OSStructures.OSMap.CircleCoordinates {
-            return {
-                coordinates: {
-                    Lat: this.providerCenter.lat,
-                    Lng: this.providerCenter.lng
-                },
-                location: {
-                    location: `${this.providerCenter.lat.toString()},${this.providerCenter.lng.toString()}`,
-                    radius: this.providerRadius
-                }
-            };
-        }
-
         public build(): void {
             super.build();
             // First build center coordinates based on the location
