dee6b7b99f99129f303ee41a3b3656805ad0161d
rug <rmfgoncalves@gmail.com>
2024-01-31 14:39:16 +0100

    Updating new configs

--- src/Providers/Maps/Google/Configuration/MarkerClusterer/MarkerClustererConfig.ts
+++ src/Providers/Maps/Google/Configuration/MarkerClusterer/MarkerClustererConfig.ts
@@ -4,32 +4,46 @@ namespace Provider.Maps.Google.Configuration.MarkerClusterer {
         extends OSFramework.Maps.Configuration.AbstractConfiguration
         implements OSFramework.Maps.Configuration.IConfigurationMarkerClusterer
     {
+        private _map: Provider.Maps.Google.OSMap.IMapGoogle;
         public clusterClass: string;
         public markerClustererActive: boolean;
         public markerClustererMaxZoom: number;
         public markerClustererMinClusterSize: number;
         public markerClustererZoomOnClick: boolean;
         public styles: Array<OSFramework.Maps.OSStructures.Clusterer.Style>;
+		
+        constructor(
+            config: OSFramework.Maps.Configuration.IConfigurationMarkerClusterer,
+            map: Provider.Maps.Google.OSMap.IMapGoogle
+        ) {
+            super(config);
+			this._map = map;
+        }
 
-        // No need for constructor, as it is not doing anything. Left the constructor, to facilitade future usage.
-        // constructor(
-        //     config: OSFramework.Maps.Configuration.IConfigurationMarkerClusterer
-        // ) {
-        //     super(config);
-        // }
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        public getProviderConfig(): any {
+        public getProviderConfig(): MarkerClustererOptions {
+            const minPoints = this.markerClustererActive ? this.markerClustererMinClusterSize : Number.MAX_SAFE_INTEGER;
             // eslint-disable-next-line prefer-const
             let provider = {
-                active: this.markerClustererActive,
-                maxZoom: this.markerClustererMaxZoom || 2,
-                minClusterSize: this.markerClustererMinClusterSize,
-                zoomOnClick: this.markerClustererZoomOnClick,
-                clusterClass:
-                    OSFramework.Maps.Helper.Constants.clusterIconCSSClass,
-                styles: ClustererStyle
+                algorithm: new window.markerClusterer.SuperClusterAlgorithm({
+					maxZoom: this.markerClustererMaxZoom || 2,
+					minPoints: minPoints,
+				}),
+                algorithmOptions: undefined,
+				map: this._map.provider,
+				markers: this._map.markersReady,
+				onClusterClick: (event: google.maps.MapMouseEvent, cluster: Cluster, map: google.maps.Map) => {
+					if (this.markerClustererZoomOnClick) {
+						map.fitBounds(cluster.bounds);
+					}
+				},
+				renderer: undefined,
             };
+            //✅ active: this.markerClustererActive,
+            //✅ maxZoom: this.markerClustererMaxZoom || 2,
+            //✅ minClusterSize: this.markerClustererMinClusterSize,
+            //✅ zoomOnClick: this.markerClustererZoomOnClick,
+            // clusterClass: OSFramework.Maps.Helper.Constants.clusterIconCSSClass,
+            // styles: ClustererStyle,
 
             //Cleanning undefined properties
             Object.keys(provider).forEach((key) => {
