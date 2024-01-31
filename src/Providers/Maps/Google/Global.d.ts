637d6abe6e3fd38403e3e38e0d16085c12ee4ba1
rug <rmfgoncalves@gmail.com>
2024-01-31 14:20:21 +0100

    Adding global definitions

--- src/Providers/Maps/Google/Global.d.ts
+++ src/Providers/Maps/Google/Global.d.ts
@@ -1,10 +1,22 @@
-import OriginalMarkerClusterer from '@googlemaps/markerclustererplus';
+import {
+    MarkerClusterer as OriginalMarkerClusterer,
+    MarkerClustererOptions as OriginalMarkerClustererOptions,
+    SuperClusterAlgorithm as OriginalSuperClusterAlgorithm, Cluster as OriginalCluster
+} from '@googlemaps/markerclusterer';
 
 declare global {
+    //Adding object that will be available globally in runtime.
     interface Window {
         GMCB: () => void;
-        MarkerClusterer: typeof OriginalMarkerClusterer; 
+        markerClusterer: {
+            MarkerClusterer: typeof OriginalMarkerClusterer,
+            SuperClusterAlgorithm: typeof OriginalSuperClusterAlgorithm,
+        }; 
     }
+    //The types below, are useful for TypeScript intellisense.
+    type Cluster = OriginalCluster;
     type MarkerClusterer = OriginalMarkerClusterer;
+    type MarkerClustererOptions = OriginalMarkerClustererOptions;
+    type SuperClusterAlgorithm = OriginalSuperClusterAlgorithm;
 }
 window.GMCB = window.GMCB || {};
