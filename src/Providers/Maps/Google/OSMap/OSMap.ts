49650cfa1a0122f8d9d375663680ffdb0a8817af
rug <rmfgoncalves@gmail.com>
2024-02-01 13:15:46 +0100

    Removing styles

--- src/Providers/Maps/Google/OSMap/OSMap.ts
+++ src/Providers/Maps/Google/OSMap/OSMap.ts
@@ -169,7 +169,7 @@ namespace Provider.Maps.Google.OSMap {
 
             // OnEventTriggered Event (other events that can be set on the advancedFormat of the Map)
             // We are deprecating the advancedFormat
-            // TO BE REMOVED SOON
+            // TODO: TO BE REMOVED SOON
             if (
                 this.mapEvents.hasHandlers(
                     OSFramework.Maps.Event.OSMap.MapEventType.OnEventTriggered
