e013e5b26f0f5889bfa1ab4dfbb64139afcc18c9
rug <rmfgoncalves@gmail.com>
2025-06-11 17:27:32 +0200

    Adding new library

--- src/Providers/Maps/Google/Constants/Constants.ts
+++ src/Providers/Maps/Google/Constants/Constants.ts
@@ -45,7 +45,7 @@ namespace Provider.Maps.Google.Constants {
 	// In order to use the drawingTools we need to add it into the libraries via the URL = drawing
 	// In order to use the heatmap we need to add it into the libraries via the URL = visualization
 	// In order to use the searchplaces we need to add it into the libraries via the URL = places (in case the Map is the first to import the scripts)
-	export const GoogleMapsLibraries = 'drawing,visualization,places,marker';
+	export const GoogleMapsLibraries = 'drawing,visualization,places,marker,geometry';
 	// Used to check if modules are available on cycles of 100ms */
 	export const checkGoogleMapsLibrariesMaxAttempts = 25;
 	// Version of the Google Maps to be loaded.
