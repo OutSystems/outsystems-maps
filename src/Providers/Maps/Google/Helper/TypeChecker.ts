097a27e4069be5000317180037e8686df1706681
rug <rmfgoncalves@gmail.com>
2025-06-11 17:27:55 +0200

    Adding new type checker method

--- src/Providers/Maps/Google/Helper/TypeChecker.ts
+++ src/Providers/Maps/Google/Helper/TypeChecker.ts
@@ -11,4 +11,16 @@ namespace Provider.Maps.Google.Helper.TypeChecker {
 	export function IsAdvancedMarker(marker: unknown): boolean {
 		return marker instanceof google.maps.marker.AdvancedMarkerElement;
 	}
+
+	/**
+	 * Validates if a string is a set of coordinates
+	 *
+	 * @export
+	 * @param {string} coordinates Coordinates to validate
+	 * @return {*}  {boolean} True if the coordinates are valid, false otherwise
+	 *
+	 */
+	export function IsValidCoordinates(coordinates: string): boolean {
+		return Constants.coordinateValidator.test(coordinates);
+	}
 }
