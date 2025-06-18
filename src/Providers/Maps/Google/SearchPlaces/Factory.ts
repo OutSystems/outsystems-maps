0bb2307c086a8f52e3992cf36026b55c60cddf5d
rug <rmfgoncalves@gmail.com>
2025-06-18 12:51:59 +0200

    Setting up default behavior

--- src/Providers/Maps/Google/SearchPlaces/Factory.ts
+++ src/Providers/Maps/Google/SearchPlaces/Factory.ts
@@ -5,13 +5,14 @@ namespace Provider.Maps.Google.SearchPlaces {
 			searchPlacesId: string,
 			configs: JSON,
 			searchPlacesVersion: string
-			
 		): OSFramework.Maps.SearchPlaces.ISearchPlaces {
 			switch (searchPlacesVersion) {
 				case OSFramework.Maps.Enum.GoogleSearchPlacesApiVersion.v1:
 					return new SearchPlaces(searchPlacesId, configs);
 				case OSFramework.Maps.Enum.GoogleSearchPlacesApiVersion.v2:
 					return new SearchPlacesV2(searchPlacesId, configs);
+				default:
+					throw new Error(`There is no factory for this Google Places API version (${searchPlacesVersion})`);
 			}
 		}
 	}
