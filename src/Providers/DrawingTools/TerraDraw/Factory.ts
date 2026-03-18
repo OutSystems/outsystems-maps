f44fb33576e7c38887329aaa7cb2d631aed580ff
rug <rmfgoncalves@gmail.com>
2026-03-18 17:30:18 +0100

    Removing other drawing tools

--- src/Providers/DrawingTools/TerraDraw/Factory.ts
+++ src/Providers/DrawingTools/TerraDraw/Factory.ts
@@ -19,18 +19,19 @@ namespace Provider.DrawingTools.TerraDraw {
 			configs: JSON
 		): OSFramework.Maps.DrawingTools.ITool {
 			switch (type) {
-				case OSFramework.Maps.Enum.DrawingToolsTypes.Marker:
-					return new DrawMarker(map, drawingTools, toolId, type, configs);
+				// case OSFramework.Maps.Enum.DrawingToolsTypes.Marker:
+				// 	return new DrawMarker(map, drawingTools, toolId, type, configs);
 				case OSFramework.Maps.Enum.DrawingToolsTypes.Polyline:
 					return new DrawPolyline(map, drawingTools, toolId, type, configs);
-				case OSFramework.Maps.Enum.DrawingToolsTypes.Polygon:
-					return new DrawPolygon(map, drawingTools, toolId, type, configs);
-				case OSFramework.Maps.Enum.DrawingToolsTypes.Circle:
-					return new DrawCircle(map, drawingTools, toolId, type, configs);
-				case OSFramework.Maps.Enum.DrawingToolsTypes.Rectangle:
-					return new DrawRectangle(map, drawingTools, toolId, type, configs);
+				// case OSFramework.Maps.Enum.DrawingToolsTypes.Polygon:
+				// 	return new DrawPolygon(map, drawingTools, toolId, type, configs);
+				// case OSFramework.Maps.Enum.DrawingToolsTypes.Circle:
+				// 	return new DrawCircle(map, drawingTools, toolId, type, configs);
+				// case OSFramework.Maps.Enum.DrawingToolsTypes.Rectangle:
+				// 	return new DrawRectangle(map, drawingTools, toolId, type, configs);
 				default:
-					throw new Error(`There is no TerraDraw factory for tool type: ${type}`);
+					//throw new Error(`There is no TerraDraw factory for tool type: ${type}`);
+					console.warn(`The tool type: ${type} is not yet available...`);
 			}
 		}
 	}
