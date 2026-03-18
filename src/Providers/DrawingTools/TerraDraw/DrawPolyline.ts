27a7176acb008c169ea0ea287922fb074c780ef2
rug <rmfgoncalves@gmail.com>
2026-03-18 17:33:50 +0100

    Adding Polyline and configs

--- src/Providers/DrawingTools/TerraDraw/DrawPolyline.ts
+++ src/Providers/DrawingTools/TerraDraw/DrawPolyline.ts
@@ -1,8 +1,9 @@
 /// <reference path="AbstractDrawPolyshape.ts" />
+/// <reference path="Configuration/DrawPolylineConfig.ts" />
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Provider.DrawingTools.TerraDraw {
-	export class DrawPolyline extends AbstractDrawPolyshape<Configuration.DrawBasicShapeConfig> {
+	export class DrawPolyline extends AbstractDrawPolyshape<Configuration.DrawPolylineConfig> {
 		constructor(
 			map: OSFramework.Maps.OSMap.IMap,
 			drawingTools: OSFramework.Maps.DrawingTools.IDrawingTools,
@@ -10,26 +11,13 @@ namespace Provider.DrawingTools.TerraDraw {
 			type: string,
 			configs: JSON
 		) {
-			super(
-				map,
-				drawingTools,
-				drawingToolsId,
-				type,
-				new Configuration.DrawBasicShapeConfig(configs)
-			);
+			super(map, drawingTools, drawingToolsId, type, new Configuration.DrawPolylineConfig(configs));
 		}
 
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		protected applyStyleChange(propertyName: string, value: unknown): void {
 			const draw = this.drawingTools.provider as TerraDrawInstance;
-			const propValue = OSFramework.Maps.Enum.OS_Config_Shape[propertyName];
-			const styleMap: Record<number, Record<string, unknown>> = {
-				[OSFramework.Maps.Enum.OS_Config_Shape.strokeColor]: { lineStringColor: value },
-				[OSFramework.Maps.Enum.OS_Config_Shape.strokeWeight]: { lineStringWidth: value },
-				[OSFramework.Maps.Enum.OS_Config_Shape.strokeOpacity]: { lineStringOpacity: value },
-			};
-			if (styleMap[propValue]) {
-				draw.updateModeOptions(this.terraDrawModeName, { styles: styleMap[propValue] });
-			}
+			draw.updateModeOptions(this.drawModeName, this.config.getProviderConfig());
 		}
 
 		protected get completedToolEventName(): string {
@@ -39,28 +27,25 @@ namespace Provider.DrawingTools.TerraDraw {
 		protected createElement(
 			uniqueId: string,
 			feature: TerraDrawGeoJSONFeature,
-			configs: Configuration.DrawBasicShapeConfig
+			configs: OSFramework.Maps.Configuration.IConfigurationShape
 		): OSFramework.Maps.Shape.IShape {
 			const locations = this.extractLocations(feature);
-			const finalConfigs = { ...configs, locations: JSON.stringify(locations) };
+			const finalConfigs = {
+				...configs,
+				locations: JSON.stringify(locations),
+			};
 			return super.createShapeElement(uniqueId, OSFramework.Maps.Enum.ShapeType.Polyline, finalConfigs);
 		}
 
 		public createTerraDrawMode(): TerraDrawBaseDrawMode {
-			return new globalThis.terraDraw.TerraDrawLineStringMode({
-				styles: {
-					lineStringColor: (this.config.strokeColor ?? '#555555') as TerraDrawHexColor,
-					lineStringWidth: this.config.strokeWeight ?? 2,
-					lineStringOpacity: this.config.strokeOpacity ?? 1,
-				},
-			});
+			return new globalThis.terraDraw.TerraDrawLineStringMode(this.config.getProviderConfig());
 		}
 
-		public get options(): Configuration.DrawBasicShapeConfig {
+		public get options(): Configuration.DrawPolylineConfig {
 			return this.config;
 		}
 
-		public get terraDrawModeName(): string {
+		public get drawModeName(): string {
 			return Constants.ModeName.LineString;
 		}
 	}
