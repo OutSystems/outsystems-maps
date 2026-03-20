c143dc2681c6fddc1143167ab093db46d97387eb
rug <rmfgoncalves@gmail.com>
2026-03-20 13:08:17 +0100

    Adding new constant and changing format

--- src/Providers/DrawingTools/TerraDraw/Constants.ts
+++ src/Providers/DrawingTools/TerraDraw/Constants.ts
@@ -1,17 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Provider.DrawingTools.TerraDraw.Constants {
-    export const enum ModeName {
-        Circle = 'circle',
-        LineString = 'linestring',
-        Marker = 'marker',
-        Polygon = 'polygon',
-        Rectangle = 'rectangle'
-    }
-    export const enum ShapeType {
-        Circle = 'Circle',
-        LineString = 'LineString',
-        Marker = 'Marker',
-        Polygon = 'Polygon',
-        Rectangle = 'Rectangle'
-    }
+	export const enum ModeName {
+		Circle = 'circle',
+		LineString = 'linestring',
+		Marker = 'marker',
+		Polygon = 'polygon',
+		Rectangle = 'rectangle',
+	}
+	export const enum ShapeType {
+		Circle = 'Circle',
+		LineString = 'LineString',
+		Marker = 'Marker',
+		Polygon = 'Polygon',
+		Rectangle = 'Rectangle',
+	}
+
+	/** DOM id used to guard against injecting the stylesheet more than once. */
+	export const drawingToolsCssId = 'os-terradraw-styles';
 }
