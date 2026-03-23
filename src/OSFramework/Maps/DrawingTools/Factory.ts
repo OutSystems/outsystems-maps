4c0fe15ad094a8b7a9e03838bf846098b7b5787d
rug <rmfgoncalves@gmail.com>
2026-03-23 19:08:21 +0100

    Support TerraDraw and Google Drawing Tools

--- src/OSFramework/Maps/DrawingTools/Factory.ts
+++ src/OSFramework/Maps/DrawingTools/Factory.ts
@@ -4,24 +4,27 @@ namespace OSFramework.Maps.DrawingTools {
 		export function MakeDrawingTools(
 			map: OSMap.IMap,
 			drawingToolsId: string,
-			configs: JSON
-		): DrawingTools.IDrawingTools {
+			configs: JSON,
+			useTerraDraw: boolean = true
+		): IDrawingTools {
+			let drawingToolsBuilder: OSFramework.Maps.Callbacks.Generic;
+
 			switch (map.providerType) {
 				case Enum.ProviderType.Google:
-					return Provider.DrawingTools.TerraDraw.DrawingToolsFactory.MakeDrawingTools(
-						map,
-						drawingToolsId,
-						configs
-					);
+					if (useTerraDraw) {
+						drawingToolsBuilder = Provider.DrawingTools.TerraDraw.DrawingToolsFactory.MakeDrawingTools;
+					} else {
+						drawingToolsBuilder = Provider.Maps.Google.DrawingTools.DrawingToolsFactory.MakeDrawingTools;
+					}
+					break;
 				case Enum.ProviderType.Leaflet:
-					return Provider.Maps.Leaflet.DrawingTools.DrawingToolsFactory.MakeDrawingTools(
-						map,
-						drawingToolsId,
-						configs
-					);
+					drawingToolsBuilder = Provider.Maps.Leaflet.DrawingTools.DrawingToolsFactory.MakeDrawingTools;
+					break;
 				default:
 					throw new Error(`There is no factory for the DrawingTools using the provider ${map.providerType}`);
 			}
+
+			return drawingToolsBuilder(map, drawingToolsId, configs) as IDrawingTools;
 		}
 
 		export function MakeTool(
@@ -29,30 +32,29 @@ namespace OSFramework.Maps.DrawingTools {
 			drawingTools: DrawingTools.IDrawingTools,
 			toolId: string,
 			type: Enum.DrawingToolsTypes,
-			configs: JSON
-		): DrawingTools.ITool {
+			configs: JSON,
+			useTerraDraw: boolean = true
+		): ITool {
+			let toolBuilder: OSFramework.Maps.Callbacks.Generic;
+
 			switch (map.providerType) {
 				case Enum.ProviderType.Google:
-					return Provider.DrawingTools.TerraDraw.DrawingToolsFactory.MakeTool(
-						map,
-						drawingTools,
-						toolId,
-						type,
-						configs
-					);
+					if (useTerraDraw) {
+						toolBuilder = Provider.DrawingTools.TerraDraw.DrawingToolsFactory.MakeTool;
+					} else {
+						toolBuilder = Provider.Maps.Google.DrawingTools.DrawingToolsFactory.MakeTool;
+					}
+					break;
 				case Enum.ProviderType.Leaflet:
-					return Provider.Maps.Leaflet.DrawingTools.DrawingToolsFactory.MakeTool(
-						map,
-						drawingTools,
-						toolId,
-						type,
-						configs
-					);
+					toolBuilder = Provider.Maps.Leaflet.DrawingTools.DrawingToolsFactory.MakeTool;
+					break;
 				default:
 					throw new Error(
 						`There is no factory for the Tool (${type}) using the provider ${map.providerType}`
 					);
 			}
+
+			return toolBuilder(map, drawingTools, toolId, type, configs) as ITool;
 		}
 	}
 }
