d7ea5d9f9370ee2dab98698b2d90c986fea7a8ef
rug <rmfgoncalves@gmail.com>
2026-03-31 17:45:15 +0200

    Remove unneeded property

--- src/Providers/Layers/deck.gl/Configuration/HeatmapLayer/IConfigurationHeatmapLayer.ts
+++ src/Providers/Layers/deck.gl/Configuration/HeatmapLayer/IConfigurationHeatmapLayer.ts
@@ -3,7 +3,6 @@ namespace Provider.Layers.deckgl.Configuration.HeatmapLayer {
 	export interface IConfigurationDeckglHeatmapLayer
 		extends OSFramework.Maps.Configuration.IConfigurationHeatmapLayer {
 		gradient: Array<OSFramework.Maps.OSStructures.HeatmapLayer.Color>;
-		id?: string;
 		minIntensity: number;
 	}
 }
