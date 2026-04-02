419d7a7aedb64c7d3f1d57b7eb5de68990629ddc
David Lourenço <david.lourenco@outsystems.com>
2026-04-02 15:00:33 +0100

    Ensure mode is only set when the tool is ready

--- src/Providers/DrawingTools/TerraDraw/DrawingTools.ts
+++ src/Providers/DrawingTools/TerraDraw/DrawingTools.ts
@@ -228,13 +228,13 @@ namespace Provider.DrawingTools.TerraDraw {
 		public removeTool(toolId: string): void {
 			const tool = this.getTool(toolId);
 
-			this._provider.setMode(Constants.ModeName.Select);
-			
 			super.removeTool(toolId);
 
 			tool && this._modeToTool.delete(tool.type);
 
 			if (this.isReady) {
+				this._provider.setMode(Constants.ModeName.Select);
+
 				this._buildTerraDraw();
 
 				this._provider.start();
