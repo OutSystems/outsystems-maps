5d537c67aca0ec3d23d088380aaf007a6e4c9d4e
rug <rmfgoncalves@gmail.com>
2026-06-01 17:54:23 +0200

    Remove task identifier

--- src/Providers/Layers/deck.gl/Constants.ts
+++ src/Providers/Layers/deck.gl/Constants.ts
@@ -4,7 +4,7 @@ namespace Provider.Layers.deckgl.Constants {
 	/** Default icon key used when no specific icon is provided. */
 	export const DEFAULT_ICON_KEY = '__default__';
 	/** Default icon URL used when no specific icon is provided. */
-	export const DEFAULT_ICON_URL = 'img/OutSystemsMaps_ROU12823.iconMarker.png';
+	export const DEFAULT_ICON_URL = 'img/OutSystemsMaps.iconMarker.png';
 	/** Default stroke color with 100% opacity if not specified in the KML properties. */
 	export const DEFAULT_STROKE_COLOR: [number, number, number, number] = [66, 133, 244, 255];
 	/** Size of each icon cell in the atlas. */
